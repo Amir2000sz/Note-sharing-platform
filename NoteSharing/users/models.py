bd44daf3e4067276aa7e6320999bfac759864b82
AmirrezaSalimzadeh <maktabamirreza@gmail.com>
2025-10-09 23:25:21 +0330

    the initialization of the project just the models and structures are built

---
+++ NoteSharing/users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    Display_name = models.CharField(max_length=200,unique=True)
+    name = models.CharField(max_length=200,blank=True,null=True)
+    family = models.CharField(max_length=200,blank=True,null=True)
+    number = models.CharField(max_length=20,unique=True,blank=True,null=True)
+    bio = models.TextField(blank=True,null=True)
+    email = models.EmailField(max_length=350)
+    ProfileImage = models.ImageField(upload_to='profilePics/',blank=True,null=True)
+    createdDate = models.DateField(auto_created=True)
+    lastSeen = models.DateTimeField(auto_now_add=True)
+
+class UserTag(models.Model):
+    title = models.CharField(max_length=30)
+    profile = models.ManyToManyField(Profile)
